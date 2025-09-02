bae7e4a5e3f5b4e3189fc9060265430eee33964f
vikashyadav-07 <vikash1.kumar22b@iiitg.ac.in>
2025-09-02 17:56:37 +0530

    frontend updsated

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -18,7 +18,7 @@ const MovieCard = ({movie}) => {
        <p className='font-semibold mt-2 truncate'>{movie.title}</p>
 
        <p className='text-sm text-gray-400 mt-2'>
-        {new Date(movie.release_date).getFullYear()} • {movie.genres.slice(0,2).map(genre => genre.name).join(" | ")} • {timeFormat(movie.runtime)}
+        {new Date(movie.release_date).getFullYear()} • {movie.genres && Array.isArray(movie.genres) ? movie.genres.slice(0,2).map(genre => genre.name).join(" | ") : ''} • {timeFormat(movie.runtime)}
        </p>
 
        <div className='flex items-center justify-between mt-4 pb-3'>
@@ -26,7 +26,7 @@ const MovieCard = ({movie}) => {
 
         <p className='flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1'>
             <StarIcon className="w-4 h-4 text-primary fill-primary"/>
-            {movie.vote_average.toFixed(1)}
+            {movie.vote_average ? movie.vote_average.toFixed(1) : ''}
         </p>
        </div>
 
